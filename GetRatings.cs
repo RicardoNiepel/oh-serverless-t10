5765eb11783a2ba8e316ca3b8b4ebc16b9c8843b
Ricardo Niepel <ricardo.niepel@outlook.com>
2022-01-18 14:43:53 +0100

    finish CosmosDB integration

--- GetRatings.cs
+++ GetRatings.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace T10Company.Function
 {
@@ -15,21 +16,15 @@ namespace T10Company.Function
         [FunctionName("GetRatings")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "teamtendatabase",
+                collectionName: "RatingItems",
+                ConnectionStringSetting = "CosmosDBConnection",
+                SqlQuery = "SELECT top 100 * FROM r ORDER BY r._ts DESC")]
+                IEnumerable<dynamic> ratingItems,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(ratingItems);
         }
     }
 }
