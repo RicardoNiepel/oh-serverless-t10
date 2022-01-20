f02e5201cb324a3164a99f5986eac8f1e9a1a022
Ricardo Niepel <ricardo.niepel@outlook.com>
2022-01-20 10:44:21 +0100

    first version but with bug

--- orderProcessing.cs
+++ orderProcessing.cs
@@ -1,5 +1,9 @@
 using System;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -9,9 +13,40 @@ namespace OH.Challenge6
     public class orderProcessing
     {
         [FunctionName("orderProcessing")]
-        public void Run([BlobTrigger("orders/{name}", Connection = "team10ohchallenge6orders_STORAGE")]Stream myBlob, string name, ILogger log)
+        public async Task Run(
+            [BlobTrigger("orders/{id}-{type}.csv", Connection = "team10ohchallenge6orders_STORAGE")]Stream myBlob, 
+            [Blob("orders/{id}-OrderHeaderDetails.csv", FileAccess.ReadWrite, Connection = "team10ohchallenge6orders_STORAGE")] ICloudBlob OrderHeaderDetails,
+            [Blob("orders/{id}-OrderLineItems.csv", FileAccess.ReadWrite, Connection = "team10ohchallenge6orders_STORAGE")] ICloudBlob OrderLineItems,
+            [Blob("orders/{id}-ProductInformation.csv", FileAccess.ReadWrite, Connection = "team10ohchallenge6orders_STORAGE")] ICloudBlob ProductInformation,
+            string id, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{id}");
+
+            if (OrderHeaderDetails != null && OrderLineItems != null && ProductInformation != null)
+            {
+                log.LogInformation($"All files there for {id}");
+                
+                var request = new 
+                {
+                    orderHeaderDetailsCSVUrl = OrderHeaderDetails.StorageUri.PrimaryUri.ToString(),
+                    orderLineItemsCSVUrl =  OrderLineItems.StorageUri.PrimaryUri.ToString(),
+                    productInformationCSVUrl =  ProductInformation.StorageUri.PrimaryUri.ToString(),
+                };
+
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await client.PostAsJsonAsync("https://serverlessohmanagementapi.trafficmanager.net/api/order/combineOrderContent", request);
+
+                var sample = await response.RequestMessage.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    log.LogError(await response.Content.ReadAsStringAsync());
+                    response.EnsureSuccessStatusCode();
+                }
+
+                log.LogInformation($"Combined order for {id}");
+            }
         }
     }
 }
